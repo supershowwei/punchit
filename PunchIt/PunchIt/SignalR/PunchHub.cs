933b390ac4bf87d888b2aefcbb489978bb831973
supershowwei <supershowwei@gmail.com>
2016-06-22 10:51:53 +0800

    show online members

--- PunchIt/PunchIt/SignalR/PunchHub.cs
+++ PunchIt/PunchIt/SignalR/PunchHub.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNet.SignalR;
 
 namespace PunchIt.SignalR
 {
     public class PunchHub : Hub
     {
+        public static HashSet<string> Connections = new HashSet<string>();
+
         public void Punch()
         {
             Clients.All.punch();
@@ -17,6 +16,20 @@ namespace PunchIt.SignalR
         public void Punched()
         {
             Clients.All.punched();
+        }
+
+        public override Task OnConnected()
+        {
+            Connections.Add(this.Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Connections.Remove(this.Context.ConnectionId);
+
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
